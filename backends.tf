8d602371e31c4f26828704d15a0b60de889a0cdc
ksista <kalyan.sista@digitalonus.com>
2020-06-24 11:43:45 -0400

    removed backend state file

--- backends.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tf-challenge-remotestate-rg"
-    storage_account_name = "tfchallengeremotestate"
-    container_name       = "tfchallenge-tfstate"
-    key                  = "core.tfchallenge.tfstate"
-  }
-}
