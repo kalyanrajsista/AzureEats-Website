f570f4908038a68248c6f8f0ddd1ca144623ffcc
ksista <kalyan.sista@digitalonus.com>
2020-06-24 13:47:57 -0400

    troubleshooting

--- providers.tf
+++ providers.tf
@@ -1,3 +1,10 @@
+terraform {
+  required_version = "~> 0.12.25"
+  required_providers {
+    azurerm = "~> 2.15"
+  }
+}
+
 # Configure the Azure Resource Manager Provider
 provider "azurerm" {
   features {}
