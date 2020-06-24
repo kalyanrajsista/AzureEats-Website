e8a1a6e24dba49b7a9af1661f35a94261c17fe64
ksista <kalyan.sista@digitalonus.com>
2020-06-24 15:41:45 -0400

    use terraform outputs

--- outputs.tf
+++ outputs.tf
@@ -30,6 +30,7 @@ output "sql_server_fqdn" {
 output "sql_connection_string" {
   description = "Connection string for the Azure SQL Database created."
   value       = "Server=tcp:${azurerm_sql_server.server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.db.name};Persist Security Info=False;User ID=${azurerm_sql_server.server.administrator_login};Password=${azurerm_sql_server.server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  sensitive   = true
 }
 
 output "MongoConnectionString" {
