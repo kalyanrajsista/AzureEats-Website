8f412a9297bc2d1212e2e9e646748c800a9ec191
ksista <kalyan.sista@digitalonus.com>
2020-06-24 11:48:16 -0400

    adjusted providers file

--- providers.tf
+++ providers.tf
@@ -1,9 +1,4 @@
 # Configure the Azure Resource Manager Provider
 provider "azurerm" {
-  subscription_id = var.azurerm_subscription_id
-  client_id       = var.azurerm_client_id
-  client_secret   = var.azurerm_client_secret
-  tenant_id       = var.azurerm_tenant_id
-
   features {}
 }
